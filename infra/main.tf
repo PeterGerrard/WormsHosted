3c02211b07a72843eede1926faa8ac5119447f19
Peter Gerrard <peter.gerrard@red-gate.com>
2020-11-06 15:14:42 +0000

    Switch to terraform

---
+++ infra/main.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  type    = string
+  default = "worms-hosted"
+}
+
+provider "azurerm" {
+  version = "2.35.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "worms-hosted"
+    storage_account_name = "wormshostedtfstate"
+    container_name       = "terraform-state"
+    key                  = "tfstate"
+  }
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_storage_account" "example" {
+  name                      = "wormshosted"
+  resource_group_name       = data.azurerm_resource_group.rg.name
+  location                  = data.azurerm_resource_group.rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  enable_https_traffic_only = true
+
+  static_website {
+    index_document = "index.html"
+  }
+}
