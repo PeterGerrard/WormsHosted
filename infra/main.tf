c70807e119a82cc68da1fa2bf4840dd177690f85
Peter Gerrard <peter.gerrard@red-gate.com>
2020-12-04 14:06:01 +0000

    Add function app

--- infra/main.tf
+++ infra/main.tf
@@ -75,3 +75,24 @@ resource "github_actions_secret" "deploy_secret" {
 }
 EOF
 }
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-consumption-plan"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                       = "wormshosted-add-game"
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+}
